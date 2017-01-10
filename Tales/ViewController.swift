7741815300ddec8cf9925342567ed987e2e13474
Jesse Tellez <jessetellez@jesses-macbook-pro-2.local>
2017-01-09 23:05:15 -0700

    Initial Commit

---
+++ Tales/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tales
+//
+//  Created by Jesse Tellez on 1/9/17.
+//  Copyright © 2017 Tales. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
